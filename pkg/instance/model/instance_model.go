a14067485997a36b07bd4d0d8ca40b66544148ba
Davidson Gomes <davidsongviolao@gmail.com>
2025-07-31 13:22:41 -0300

    chore: update Docker image workflow and enhance instance model
    - Removed outdated semantic versioning patterns from the Docker image publishing workflow to streamline the tagging process.
    - Added a `CreatedAt` field to the `Instance` model, which automatically captures the creation timestamp, improving data tracking and management.
    - This update simplifies the Docker image workflow and enhances the instance model's capabilities, ensuring better data integrity and operational efficiency.

--- pkg/instance/model/instance_model.go
+++ pkg/instance/model/instance_model.go
@@ -1,27 +1,30 @@
 package instance_model
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type Instance struct {
-	Id               string `json:"id" gorm:"type:uuid;primaryKey"`
-	Name             string `json:"name"`
-	Token            string `json:"token" gorm:"unique"`
-	Webhook          string `json:"webhook"`
-	RabbitmqEnable   string `json:"rabbitmqEnable"`
-	WebSocketEnable  string `json:"websocketEnable"`
-	NatsEnable       string `json:"natsEnable"`
-	Jid              string `json:"jid" gorm:"column:jid"`
-	Qrcode           string `json:"qrcode" gorm:"type:text"`
-	Connected        bool   `json:"connected"`
-	Expiration       int64  `json:"expiration"`
-	DisconnectReason string `json:"disconnect_reason"`
-	Events           string `json:"events"`
-	OsName           string `json:"os_name"`
-	Proxy            string `json:"proxy"`
-	ClientName       string `json:"client_name"`
+	Id               string    `json:"id" gorm:"type:uuid;primaryKey"`
+	Name             string    `json:"name"`
+	Token            string    `json:"token" gorm:"unique"`
+	Webhook          string    `json:"webhook"`
+	RabbitmqEnable   string    `json:"rabbitmqEnable"`
+	WebSocketEnable  string    `json:"websocketEnable"`
+	NatsEnable       string    `json:"natsEnable"`
+	Jid              string    `json:"jid" gorm:"column:jid"`
+	Qrcode           string    `json:"qrcode" gorm:"type:text"`
+	Connected        bool      `json:"connected"`
+	Expiration       int64     `json:"expiration"`
+	DisconnectReason string    `json:"disconnect_reason"`
+	Events           string    `json:"events"`
+	OsName           string    `json:"os_name"`
+	Proxy            string    `json:"proxy"`
+	ClientName       string    `json:"client_name"`
+	CreatedAt        time.Time `json:"createdAt" gorm:"autoCreateTime"`
 
 	// Advanced Settings
 	AlwaysOnline  bool   `json:"alwaysOnline" gorm:"default:false"`
